4871ce86c17f1e55fdce8718e183a4b08010ec58
johnemmanueldagsa-eng <johnemmanuel.dagsa@gmail.com>
2026-07-03 19:37:07 +0800

    Update hero-branding.tsx

--- src/components/sections/hero-branding.tsx
+++ src/components/sections/hero-branding.tsx
@@ -48,7 +48,7 @@ const HeroBranding = () => {
             {/* Main Card Image */}
             <div className="relative z-10 w-[240px] sm:w-[320px] mx-auto transition-transform duration-500 group-hover:scale-105">
               <img 
-                src="https://i.imgur.com/SEKa08t.jpeg" 
+                src="https://i.imgur.com/iVoQ6yN.png" 
                 alt="Marshalls $750 Gift Card" 
                 className="w-full h-auto"
               />
